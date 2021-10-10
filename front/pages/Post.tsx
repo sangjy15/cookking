c8b486c0973b4da2d23231c7f1fee1341244590a
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2021-10-10 16:11:01 +0900

    webview_test

--- front/pages/Post.tsx
+++ front/pages/Post.tsx
@@ -1,13 +1,28 @@
 import React, {useState} from 'react'
 import Container from '../components/Container'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import ButtonWrap from '../components/ButtonWrap'
+import Button, { ButtonText } from '../components/Button'
+import { WebView } from 'react-native-webview'
+
+const HTML = `
+	<h1>This Is A Heading</h1>
+	<h1>This Is A Heading</h1>
+	<h1>This Is A Heading</h1>
+	`
 
 function Post() {
 	const navigation = useNavigation()
 	return (
 		<Container>
-			<Text>Post</Text>
+			<Text>tddt</Text>
+			<WebView
+				style={{flex:1}}
+				originWhitelist={['*']}
+				source={{ html: HTML }}
+			/>
+			<Text>tt</Text>
 		</Container>
 	)
 }
